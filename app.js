2b08e27ec0158c87653afc37c66dee686a9b0ce1
matin <matin@gmail.com>
2022-02-18 07:07:21 -0500

    return form data

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ document.getElementById("send").addEventListener("click", submit);
 function submit () {
     const result = new Validate(document.getElementById("data"));
 
-    if (result.ok) {
+    if (result.data) {
         // AJAX OR USE INFORMATIONS
         console.log("AJAX OR USE INFORMATIONS");
+
+        result.data.forEach(value => console.log(value));
     }
 }
--- validate.js
+++ validate.js
@@ -1,5 +1,5 @@
 class Validate {
-    ok = false;
+    ok;
     inputs = {};
     emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
     errorTooltip = document.getElementById("err-tooltip");
@@ -113,7 +113,7 @@ class Validate {
         }, 3000);
     }
 
-    validate () {
+    validate (form) {
         for (let input in this.inputs) {
             const empty = this.checkEmpty(this.inputs[input]),
                 validate = this.checkData(this.inputs[input]);
@@ -129,7 +129,7 @@ class Validate {
             }
         }
 
-        return true;
+        return new FormData(form);
     }
 
     constructor (form) {
@@ -137,6 +137,6 @@ class Validate {
             input => this.inputs[input.name] = input
         );
 
-        this.ok = this.validate();
+        this.data = this.validate(form);
     }
 }
